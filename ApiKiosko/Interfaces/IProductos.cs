1fe11b33f7c0bccbd6c9d042d5b828c4df697f1f
jestehdz <jestehdz@gmail.com>
2025-01-05 22:36:53 -0500

    se suben cambios para mejora del codigo

--- ApiKiosko/Interfaces/IProductos.cs
+++ ApiKiosko/Interfaces/IProductos.cs
@@ -4,10 +4,10 @@ namespace ApiKiosko.Interfaces
 {
     public interface IProductos
     {
-        Task<List<Productos>> GetProductos();
+        Task<List<Productos>> GetProductos(string id);
         Task<Productos> GetProductosById(int Id);
-        Task<bool> DeleteProducto(int Id);
-        Task<Productos> CreateProductos(Productos producto);
-        Task<Productos> UpdateProductos(int id,Productos producto);
+        Task<List<Productos>> DeleteProducto(List<Productos> productos);
+        Task<List<Productos>> CreateProductos(List<Productos> producto);
+        Task<List<Productos>> UpdateProductos(List<Productos> producto);
     }
 }
